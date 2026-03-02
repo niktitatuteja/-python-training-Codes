b2732cbd284508e38bcdf31d4976df316bd70058
niktitatuteja <161914835+niktitatuteja@users.noreply.github.com>
2026-03-02 15:31:11 +0530

    150. Evaluate Reverse Polish Notation

---
+++ 150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,23 @@
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token == '+':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(a + b)
+            elif token == '-':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(a - b)
+            elif token == '*':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(a * b)
+            elif token == '/':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(a / b))
+            else:
+                stack.append(int(token))
+        return stack.pop()
